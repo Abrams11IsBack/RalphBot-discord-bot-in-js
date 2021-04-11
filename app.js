637d3daee7eb19aabba4bcaefd1ffde9e94f8f71
Abrams11 <71346253+Abrams11IsBack@users.noreply.github.com>
2021-04-11 22:43:04 +0200

    first version of the bot
    can  operate one single command

---
+++ app.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Demola OS PRO v1.0.0');
+});
+client.on('message', message => {
+	console.log(message.content);
+    if (message.content.startsWith(`?warn`)) {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('PUPA');
+    }
+});
+
+
+client.login('ODMwODk2NTMwMzA2MTA1Mzk0.YHNWsg.bTwqVwuLSy3BWEH9fVHyMQzseqk');
